38f3d894f826c21bb456b70726019a88181f5f94
Sung Hyobin <hellmir610@gmail.com>
2024-02-17 17:43:21 +0900

    feat(member): 이메일 주소 변경 기능 프로덕션 코드 추가 (#33)
    - 이메일 주소 변경 요청
    - 이메일 주소 변경 비즈니스 로직
    - 204 status code 반환

---
+++ member-service/member-application/src/main/java/personal/jeuksipay/member/application/port/out/UpdateMemberPort.java
@@ -0,0 +1,7 @@
+package personal.jeuksipay.member.application.port.out;
+
+import personal.jeuksipay.member.domain.Member;
+
+public interface UpdateMemberPort {
+    void updateMember(Member member);
+}
